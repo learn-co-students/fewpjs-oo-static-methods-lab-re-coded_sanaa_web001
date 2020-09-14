dd2d8ec3f93fdd270a6fb24d1a6be138e01579bb
Somaia Abdullah <somia.abdullah3@gmail.com>
2020-09-14 23:52:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 class Formatter {
-  //add static methods here
+
+    static capitalize (string) {
+     return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+
+    static sanitize (string) {
+    return string.replace(/[^A-Za-z0-9-' ]+/g, '');
+    }
+
+    static titleize (string) {
+      let words = string.split(' ')
+      let wordsCap = words.map ((word, index) => {
+        if (word !=='the' && word !=='a' && word !=='an' &&word !=='but' && word !=='of' && word !=='and'&& word !=='for' && word !=='at' && word !=='by' && word !=='from')
+        return this.capitalize(word)
+        else if (index === 0)
+        return this.capitalize(word)
+        else
+        return word;
+      });
+     return wordsCap.join(" ");
+    }
 }
