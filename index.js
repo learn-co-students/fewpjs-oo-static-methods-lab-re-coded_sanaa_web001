bf8f4a05db9174713ec2e0821906536f50a15802
Hajar Helal <hajerhelal44@gmail.com>
2020-09-28 20:20:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 class Formatter {
-  static capitalize(stri){
-    console.log(srti.uppercasa())
+  static capitalize(stri) {
+    return stri.charAt( 0 ).toUpperCase() + stri.slice( 1 )
   }
-  //add static methods here
+
+  static sanitize(stri) {
+    return stri.replace( /[^A-Za-z0-9 '-]/g, '' )
+  }
+
+  static titleize( sentence ) {
+    let exceptions = [ 'the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from' ]
+    let result = [];
+    let arrayOfWords = sentence.split( " " )
+    for ( let n = 0; n < arrayOfWords.length; n++ ) {
+      if ( n == 0 ) {
+        result.push( this.capitalize( arrayOfWords[ n ] ) )
+      } else {
+        if ( exceptions.includes( arrayOfWords[ n ] ) ) {
+          result.push( arrayOfWords[ n ] )
+        } else {
+          result.push( this.capitalize( arrayOfWords[ n ] ) )
+        }
+      }
+
+    }
+    return result.join( " " );
+  }
 }
